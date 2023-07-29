6e10c6f394d15c02bba15e2bb0a7ba596c2dd38a
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2023-07-29 00:06:01 -0300

    Update canciones.js
    Corrección d errores en la selección de elementos

--- canciones.js
+++ canciones.js
@@ -3,7 +3,7 @@
 // Cargo todos los elementos a manipular
 const grillaDeCanciones = document.getElementById("canciones");
 // Obtengo solo enlaces de la grilla de canciones
-const enlaces = Array.from(document.querySelector("a #canciones"));
+const enlaces = Array.from(document.querySelector("a"));
 /* Establezco que cada enlace dentro de la grilla de canciones tengo de texto de lector de pantalla el titulo de cancion del atributo title
    y que cada uno aparezca al lector de pantalla como "canción" */
 enlaces.map((enlace) => enlace.setAttribute("aria-labeledby", "canción"));
