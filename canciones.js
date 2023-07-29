0c01eb40b43960efc1de19cb5a65e42967af74ac
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2023-07-28 23:53:34 -0300

    Create canciones.js
    Agregado script para que se mejore la accesibilidad sin modificar tanto código y mas adelante agregare un objeto para agregar canciones solo dando nombre, enlace e imagen en una opción menos verbosa de cuando empece que no modifico esa parte para no hacer el doble.

---
+++ canciones.js
@@ -0,0 +1,10 @@
+// Se encarga de complementar informacion de accesibilidad de las canciones y agregar tres nuevas a la grilla
+
+// Cargo todos los elementos a manipular
+const grillaDeCanciones = document.getElementById("canciones");
+// Obtengo solo enlaces de la grilla de canciones
+const enlaces = Array.from(document.querySelector("#canciones a"));
+/* Establezco que cada enlace dentro de la grilla de canciones tengo de texto de lector de pantalla el titulo de cancion del atributo title
+   y que cada uno aparezca al lector de pantalla como "canción" */
+enlaces.map((enlace) => enlace.setAttribute("aria-labeledby", "canción"));
+enlaces.map((enlace) => enlace.setAttribute("aria-lablel", enlace.title));
