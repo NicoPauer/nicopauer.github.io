e5e69cf95d2648a52bce7d7377b754d0d9f58fbb
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2023-08-10 20:01:37 -0300

    Update canciones.js
    14 canciones de 18 agregadas.

--- canciones.js
+++ canciones.js
@@ -150,6 +150,31 @@ const cancion_12 = new Cancion("Soda Stereo", "Persiana Americana");
 cancion_12.imagen = "americana.png";
 cancion_12.genero = "Rock";
 cancion_12.lanzamiento = 1986;
+cancion_12.enlace = "https://www.youtube.com/watch?v=Z3L5M7GHAkw";
+
+const cancion_13 = new Cancion("Lady Gaga", "Stupid Love");
+cancion_13.imagen = ".png";
+cancion_13.genero = "Pop";
+cancion_13.lanzamiento = 2020;
+cancion_13.enlace = "https://www.youtube.com/watch?v=5L6xyaeiV58";
+
+const cancion_14 = new Cancion("La Oreja De Van Gogh", "Cometas Por El Cielo");
+cancion_14.imagen = "cometas.jpg";
+cancion_14.genero = "Pop";
+cancion_14.lanzamiento = 2011;
+cancion_14.enlace = "https://www.youtube.com/watch?v=j7FIquNQqkc";
+
+const cancion_15 = new Cancion("", "");
+cancion_15.imagen = ".png";
+cancion_15.genero = "Pop";
+cancion_15.lanzamiento = 20;
+cancion_15.enlace = "";
+
+const cancion_16 = new Cancion("", "");
+cancion_16.imagen = ".png";
+cancion_16.genero = "Pop";
+cancion_16.lanzamiento = 20;
+cancion_16.enlace = "";
 
 // Creo arreglo con objetos que instancian a Cancion
 const canciones = [cancion_1, cancion_2, cancion_3, cancion_4, cancion_5, cancion_6, cancion_7, cancion_8, cancion_9, cancion_10, cancion_11, cancion_12, cancion_13, cancion_14, cancion_15, cancion_16, cancion_17, cancion_18];
